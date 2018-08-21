fbb61cd8485129c1c24e6aab237bb044aee8d55b
bennitalg <bennitalg@outlook.com>
2018-08-20 22:49:44 -0500

    Add project files.

---
+++ CG4-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
